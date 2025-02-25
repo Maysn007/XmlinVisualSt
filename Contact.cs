0f97a729ef2fe01de2167101314c314074b4eff1
Mathias Junger <MJunger@schueler.hakzell.at>
2025-02-25 12:14:50 +0100

    add func

---
+++ Contact.cs
@@ -0,0 +1,17 @@
+﻿namespace ContactApp;
+
+public class Contact
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Phone { get; set; }
+    public string Mail { get; set; }
+
+    public Contact(string firstname, string lastname, string phone)
+    {
+        this.FirstName = firstname;
+        this.LastName = lastname;
+        this.Phone = phone;
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -1,21 +1,29 @@
-﻿using System.Xml.Linq;
+﻿using ContactApp;
+using System.Xml.Linq;
 
 try
 {
     XElement rootElement = XElement.Load("contacts.xml");
 
     var contacts = from contact in rootElement.Descendants("contact")
-                   select contact;
+                   select new Contact(contact.Attribute("firstname").Value,
+                   contact.Attribute("lastname").Value,
+                   contact.Attribute("phone").Value)
+                   { Mail = contact.Attribute("mail").Value};
 
-    foreach ( var contact in contacts)
+    foreach (var contact in contacts)
     {
-        Console.WriteLine(contact.Attribute("firstname").Value);
+        Console.WriteLine(contact.FirstName);
+        Console.WriteLine(contact.LastName);
+        Console.WriteLine(contact.Phone);
+        Console.WriteLine(contact.Mail);
+        Console.WriteLine();
     }
 
 }
 catch (Exception ex)
 {
     Console.WriteLine(ex.Message);
-	throw;
+    throw;
 }
 
