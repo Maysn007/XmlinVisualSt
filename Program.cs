a8d860c52a0e9926de9825c4eb63acb9fe2c68dc
Mathias Junger <MJunger@schueler.hakzell.at>
2025-02-25 12:05:38 +0100

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Xml.Linq;
+
+try
+{
+    XElement rootElement = XElement.Load("contacts.xml");
+
+    var contacts = from contact in rootElement.Descendants("contact")
+                   select contact;
+
+    foreach ( var contact in contacts)
+    {
+        Console.WriteLine(contact.Attribute("firstname").Value);
+    }
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+	throw;
+}
+
